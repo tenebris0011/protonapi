aa11167c555869d624594e798eb5fd4b4de78680
Nathanial Wilson <wilson.nathanial95@gmail.com>
2022-07-09 09:54:08 -0500

    Change API endpoint for pulling game info to cut down on app data.

--- app/main.py
+++ app/main.py
@@ -48,7 +48,6 @@ def getUserGames(steamid: int) -> dict:
 
     if data is not None:
         data = json.loads(data)
-        data["cache"] = True
         return data
     else:
         with httpx.Client() as client:
@@ -58,7 +57,6 @@ def getUserGames(steamid: int) -> dict:
             url = f"{base_url}/?key={apiKey}&steamid={str(steamid)}&format=json"
 
             data = client.get(url).json()
-            data["cache"] = False
             data = json.dumps(data)
             state = set_routes_to_cache(key=steamid, value=data)
 
@@ -70,17 +68,14 @@ def getUserGames(steamid: int) -> dict:
 def fetchGameRating(appid: int, gameName: str) -> dict:
     """Data from protondb api."""
     data = get_routes_from_cache(key=gameName)
-
     if data is not None:
         data = json.loads(data)
-        data["cache"] = True
         return data
     else:
         with httpx.Client() as client:
             try:
                 url = f"https://www.protondb.com/api/v1/reports/summaries/{str(appid)}.json"
                 data = client.get(url).json()
-                data["cache"] = False
                 data = json.dumps(data)
                 state = set_routes_to_cache(key=gameName, value=data)
 
@@ -88,7 +83,6 @@ def fetchGameRating(appid: int, gameName: str) -> dict:
                     return json.loads(data)
             except json.decoder.JSONDecodeError:
                 data = {'trendingTier': "", 'bestReportedTier': "", 'confidence': ""}
-                data["cache"] = False
                 data = json.dumps(data)
                 state = set_routes_to_cache(key=gameName, value=data)
 
@@ -105,17 +99,31 @@ def fetchGames(key: str) -> dict:
 
     if data is not None:
         data = json.loads(data)
-        data["cache"] = True
         return data
 
     else:
         with httpx.Client() as client:
-            url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
-            data = client.get(url).json()
-            data["cache"] = False
+            appId = ""
+            apiKey = os.getenv('API_KEY')
+            data = dict()
+            x = 0
+            while True:
+                url = f"https://api.steampowered.com/IStoreService/GetAppList/v1/?&include_games=true&last_appid={appId}&max_results=50000&key={apiKey}"
+            #url = "https://api.steampowered.com/ISteamApps/GetAppList/v2/"
+                appList = client.get(url).json()
+
+                for game in appList['response']['apps']:
+                    data[x] = json.dumps(game)
+                    x += 1
+                try:
+                    if appList['response']['have_more_results']:
+                        appId = appList['response']['last_appid']
+                    else:
+                        break
+                except KeyError:
+                    break
             data = json.dumps(data)
             state = set_routes_to_cache(key=key, value=data)
-
             if state is True:
                 return json.loads(data)
         return data
@@ -140,19 +148,20 @@ app = FastAPI(title="ProtonDB API")
 @app.get('/api/v1/update_cache')
 def updateGameRatingCache():
     allGames = fetchGames("games")
-    for game in allGames['applist']['apps']:
+    state = False
+
+    for item in allGames:
+        item = json.loads(allGames[item])
         try:
-            rating = fetchGameRating(game['appid'], game['name'])
+            rating = fetchGameRating(item['appid'], item['name'])
             data = {"bestReportedTier": rating['bestReportedTier'], "trendingTier": rating['trendingTier'],
                     "confidence": rating["confidence"]}
-            data["cache"] = False
             data = json.dumps(data)
-            state = set_routes_to_cache(key=game['name'], value=data)
+            state = set_routes_to_cache(key=item['name'], value=data)
         except KeyError:
             data = {'trendingTier': "", 'bestReportedTier': "", 'confidence': ""}
-            data["cache"] = False
             data = json.dumps(data)
-            state = set_routes_to_cache(key=game['name'], value=data)
+            state = set_routes_to_cache(key=item['name'], value=data)
     if state is True:
         return "Cache successfully updated"
 
@@ -164,10 +173,11 @@ def getUserGameRatings(steamid: int) -> dict:
         allGames = fetchGames("games")
         results = {}
         for game in userGames['response']['games']:
-            for item in allGames['applist']['apps']:
+            for item in allGames:
+                item = json.loads(allGames[item])
                 if game['appid'] == item['appid']:
                     rating = fetchGameRating(item['appid'], item['name'])
                     data = {item['name']: {"bestReportedTier": rating['bestReportedTier'],
-                                           "trendingTier": rating['trendingTier'], "confidence": rating["confidence"]}}
+                                            "trendingTier": rating['trendingTier'], "confidence": rating["confidence"]}}
                     results.update(data)
         return results
